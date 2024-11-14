04b2eca1229a65dd9289bb6c8ba405a900dc9d16
Murtada <murtada.altarouti@gmail.com>
2024-11-14 20:33:13 +0300

    Added templates

--- src/website/blueprints/index.py
+++ src/website/blueprints/index.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
+from flask import render_template
 
 
-index_blueprint = Blueprint("index", __name__)
+index_blueprint = Blueprint(name="index",
+                            import_name=__name__)
 
 
 @index_blueprint.route("/")
 def index() -> None:
-    return "Hello world"
+    return render_template("index.html")
