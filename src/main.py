7cdfd20ccbb8e68d02bb85bd1d4fdd7614c29fe9
Murtada Altarouti <murtada.altarouti@gmail.com>
2024-11-15 13:15:39 +0300

    Changed debugging port

--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@ from website import create_website
 
 if __name__ == "__main__":
     app = create_website()
-    app.run("0.0.0.0", port=80, debug=True)
+    app.run("0.0.0.0", port=3000, debug=True)
