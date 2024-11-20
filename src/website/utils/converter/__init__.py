1d19a53f7060d9eb0f102f45308334d63560d0e1
Murtada Altarouti <murtada.altarouti@gmail.com>
2024-11-20 18:50:02 +0300

    Completed all regex replacement in converter

--- src/website/utils/converter/__init__.py
+++ src/website/utils/converter/__init__.py
@@ -1,67 +1,68 @@
 from __future__ import annotations
 
 import re
+from uuid import uuid4
 
 
-H1_REGEX = re.compile(r'(^# )([A-Za-z0-9 ]+)', re.MULTILINE)
-H1_TAG = r'<h1 class="blogcontent-h1">\2</h1>'
+ULI = str(uuid4())
+OLI = str(uuid4())
 
-H2_REGEX = re.compile(r'(^## )([A-Za-z0-9 ]+)', re.MULTILINE)
-H2_TAG = r'<h2 class="blogcontent-h2">\2</h2>'
+H1_REGEX = re.compile(r'(^# )(.+)', re.MULTILINE)
+H1_TAG = r'<h1>\2</h1>'
 
-H3_REGEX = re.compile(r'(^### )([A-Za-z0-9 ]+)', re.MULTILINE)
-H3_TAG = r'<h3 class="blogcontent-h3">\2</h3>'
+H2_REGEX = re.compile(r'(^## )(.+)', re.MULTILINE)
+H2_TAG = r'<h2>\2</h2>'
 
-H4_REGEX = re.compile(r'(^#### )([A-Za-z0-9 ]+)', re.MULTILINE)
-H4_TAG = r'<h4 class="blogcontent-h4">\2</h4>'
+H3_REGEX = re.compile(r'(^### )(.+)', re.MULTILINE)
+H3_TAG = r'<h3>\2</h3>'
 
-H5_REGEX = re.compile(r'(^##### )([A-Za-z0-9 ]+)', re.MULTILINE)
-H5_TAG = r'<h5 class="blogcontent-h5">\2</h5>'
+H4_REGEX = re.compile(r'(^#### )(.+)', re.MULTILINE)
+H4_TAG = r'<h4>\2</h4>'
 
-H6_REGEX = re.compile(r'(^###### )([A-Za-z0-9 ]+)', re.MULTILINE)
-H6_TAG = r'<h6 class="blogcontent-h6">\2</h6>'
+H5_REGEX = re.compile(r'(^##### )(.+)', re.MULTILINE)
+H5_TAG = r'<h5>\2</h5>'
 
-OL_ITEM_REGEX = re.compile(r'^([0-9]+.\s?)(.+)', re.MULTILINE)
-OL_ITEM_TAG = r'<li class="blogcontent-ol-item">\2</li>'
+H6_REGEX = re.compile(r'(^###### )(.+)', re.MULTILINE)
+H6_TAG = r'<h6>\2</h6>'
 
-UL_ITEM_REGEX = re.compile(r'^(\*\s?)(.+)', re.MULTILINE)
-UL_ITEM_TAG = r'<li class="blogcontent-ul-item">\2</li>'
+OLI_REGEX = re.compile(r'^([0-9]+.\s?)(.+)\n', re.MULTILINE)
+OLI_TAG = rf'<{OLI}>\2</{OLI}>'
+
+ULI_REGEX = re.compile(r'^(\*\s?)(.+)\n', re.MULTILINE)
+ULI_TAG = rf'<{ULI}>\2</{ULI}>'
 
 P_REGEX = re.compile(r'(^[A-Za-z0-9].+)', re.MULTILINE)
-P_TAG = r'<p class="blogcontent-p">\1</p>'
+P_TAG = r'<p>\1</p>'
 
-STRONG_REGEX = re.compile(r'\*\*(.+)\*\*', re.MULTILINE)
-STRONG_TAG = r'<strong class="blogcontent-strong">\1</strong>'
+STRONG_REGEX = re.compile(r'\*\*(.+?)\*\*', re.MULTILINE)
+STRONG_TAG = r'<strong>\1</strong>'
 
-I_REGEX = re.compile(r'\*(.+)\*', re.MULTILINE)
-I_TAG = r'<em class="blogcontent-em">\1</em>'
+I_REGEX = re.compile(r'\*(.+?)\*', re.MULTILINE)
+I_TAG = r'<em>\1</em>'
 
 IMG_REGEX = re.compile(r'^\!\[(.+)\]\((.+)\)', re.MULTILINE)
-IMG_TAG = r'<img class="blogcontent-img" src="\2" alt="\1"/>'
+IMG_TAG = r'<img src="\2" alt="\1"/>'
 
 A_REGEX = re.compile(r'^\[(.+)\]\((.+)\)', re.MULTILINE)
-A_TAG = r'<a class="blogcontent-a" href="\2">\1</a>'
+A_TAG = r'<a href="\2">\1</a>'
 
 HR_REGEX = re.compile(r'^(___)$', re.MULTILINE)
-HR_TAG = r'<hr class="blogcontent-hr">'
+HR_TAG = r'<hr>'
 
 BR_REGEX = re.compile(r'^\s*$')
-BR_TAG = r'<br class="blogcontent-br">'
+BR_TAG = r'<br>'
 
-UL_REGEX = re.compile(r'(?:<li class="blogcontent-ul-item"[^>]*>.*?<\/li>\s*)+')  # noqa
-UL_TAG = r'<ul class="blogcontent-ul">\2</ul>'
+UL_REGEX = re.compile(rf'(<{ULI}>.*<\/{ULI}>)')
+UL_TAG = r'<ul>\1</ul>\n'
 
-OL_REGEX = re.compile(r'(?:<li class="blogcontent-ol-item"[^>]*>.*?<\/li>\s*)+')  # noqa
-OL_TAG = r'<ol class="blogcontent-ol">\2</ol>'
+OL_REGEX = re.compile(rf'(<{OLI}>.*<\/{OLI}>)')
+OL_TAG = r'<ol>\1</ol>\n'
 
 PRE_REGEX = re.compile(r'```([a-z]+)\n([\s\S]*?)\n```', re.MULTILINE)
-PRE_TAG = r'<pre language="\1">\2</pre>'
+PRE_TAG = r'<pre><code language="\1">\2</code></pre>'
 
-# TABLE_HEADER = re.compile(r'^|#(.)+?#|', re.MULTILINE)
-# TABLE_CELL = re.compile(r'', re.MULTILINE)
-
-# TABLE_REGEX = ''
-# TABLE_TAG = ''
+LIST_ITEM_REGEX = re.compile(rf'(\<\/?)({OLI}|{ULI})(\>)')
+LIST_ITEM_TAG = r'\1li\3'
 
 
 class Converter:
@@ -79,8 +80,8 @@ class Converter:
             (H5_REGEX, H5_TAG),
             (H6_REGEX, H6_TAG),
             (PRE_REGEX, PRE_TAG),
-            (OL_ITEM_REGEX, OL_ITEM_TAG),
-            (UL_ITEM_REGEX, UL_ITEM_TAG),
+            (OLI_REGEX, OLI_TAG),
+            (ULI_REGEX, ULI_TAG),
             (P_REGEX, P_TAG),
             (STRONG_REGEX, STRONG_TAG),
             (I_REGEX, I_TAG),
@@ -90,11 +91,11 @@ class Converter:
             (BR_REGEX, BR_TAG),
             (UL_REGEX, UL_TAG),
             (OL_REGEX, OL_TAG),
+            (LIST_ITEM_REGEX, LIST_ITEM_TAG),
         ]
 
     def convert(self, markdown_text: str) -> str:
         html = markdown_text
         for pattern, replacement in self._patterns:
             html = re.sub(pattern, replacement, html)
-            print(html)
         return html
